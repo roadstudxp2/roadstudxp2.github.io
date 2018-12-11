80850df06397439c1d7499ea8afe0dc5559cbf8d
zengtianshengz <zengtianshengz@163.com>
2018-12-11 20:55:20 +0800

    v2

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import { HashRouter, Link, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch, Redirect } from 'react-router-dom';
 import { parse } from 'url';
 import { homepage } from '../package.json';
 import routers from 'src/routers';
+import PageHome from 'src/pages/home';
+import PageAbout from 'src/pages/about';
 import HeaderComp from 'src/pages/components/header';
 import FooterComp from 'src/pages/components/footer';
 import './App.less';
 
 const basename = parse(homepage).pathname;
 
+const loadingComponent = () => (
+  <div className="loading-content">dskkkkkksdjdjdjdskjdk</div>
+);
+
 const App = () => (
-  <HashRouter basename={basename}>
+  <BrowserRouter basename={basename}>
     <section className="app">
       <div className="app-body">
         <HeaderComp/>
         <section className="app-content">
           <Switch>
-            <Route path="/" exact component={routers.PageHome} />
-            <Route path="/about/" component={routers.PageAbout} />
+            <Route path="/" exact component={PageHome} />
+            <Route path="/about/" component={PageAbout} />
             <Route path="/news/" component={routers.PageNews} />
             <Route path="/products/" component={routers.PageProducts} />
             <Redirect to='/'/>
@@ -26,7 +32,7 @@ const App = () => (
       </div>
       <FooterComp/>
     </section>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,12 @@
+import './setupTests.js'
 import React from 'react'
-import {
-  hydrate,
-  render
-} from 'react-dom'
+import ReactDOM from 'react-dom';
+
 import App from './App';
 import './style/common.less';
 import './style/markdown.css';
+import { render } from 'react-snapshot';
 
 const rootElement = document.getElementById('root');
 
-if (rootElement.hasChildNodes()) {
-  console.log('App: Hydrate');
-  hydrate(< App / >, rootElement);
-} else {
-  console.log('App: Render');
-  render(< App / >, rootElement);
-}
+render(<App/>, rootElement)
