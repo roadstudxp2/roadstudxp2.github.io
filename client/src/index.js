5ab54a42209d8e1b53b2adbad023a5118274b79b
zengtianshengz <zengtianshengz@163.com>
2018-12-22 10:54:26 +0800

    v3

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import './setupTests.js'
+import './registerServiceWorker'
 import React from 'react'
 import ReactDOM from 'react-dom';
-import 'nprogress/nprogress.css';// Progress 进度条样式
-
 import App from './App';
 import './style/common.less';
 import './style/markdown.css';
+import 'nprogress/nprogress.css';// Progress 进度条样式
 import { render } from 'react-snapshot';
 
 const rootElement = document.getElementById('root');
